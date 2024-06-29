cf1e7d007abfc52df28c6e60bd78fafb415351a1
ThanuraSilva <thanurasilva1989@gmail.com>
2024-06-29 15:53:49 +0530

    JSON response created via String bulder and other essentials

--- src/main/java/lk/ijse/gdse/aad67/studentmanagemant2024/controllers/StudentController.java
+++ src/main/java/lk/ijse/gdse/aad67/studentmanagemant2024/controllers/StudentController.java
@@ -1,17 +1,32 @@
 package lk.ijse.gdse.aad67.studentmanagemant2024.controllers;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/student")
 public class StudentController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
       //Todo: Save student
-
+        if(!req.getContentType().toLowerCase().startsWith("application/json")|| req.getContentType() == null){
+            //send error
+            resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
+        }
+        //process
+        BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
+        var writer = resp.getWriter();
+        reader.lines().forEach(line-> sb.append(line+"\n"));
+        System.out.println(sb);
+        writer.write(sb.toString());
+        writer.close();
     }
 
     @Override
